506d096984e492b0c6bc18e58dc3972b29c7f8d9
italianshen <747976478@qq.com>
2020-04-21 12:45:40 +0800

    【Fix】1.添加安卓预览图

--- flutter_today_news/lib/utils/net_url.dart
+++ flutter_today_news/lib/utils/net_url.dart
@@ -1,7 +1,7 @@
 class NetUrl {
   // 服务器地址
   static const String BASE_URL = "https://is.snssdk.com";
-  // 首页tab标题分类接口
+  // 首页tab标题分类接口 https://is.snssdk.com/article/category/get_subscribed/v1/
   static const String HOME_NAV_TITLE_URL = BASE_URL + "/article/category/get_subscribed/v1/";
   /// 首页新闻feednews数据接口
   static const String HOME_FEED_NEWS_DATA_URL = BASE_URL + "/api/news/feed/v58/";
